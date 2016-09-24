2bb78f43175192b175474297583648fd7d4b6df0
root <nuaays@gmail.com>
2016-09-25 00:01:18 +0800

    add example for os/exec in golang

---
+++ LearningGolang/os_exec_command.go
@@ -0,0 +1,13 @@
+//http://studygolang.com/articles/3329
+
+func main() {
+	cmd := exec.Command("tr", "a-z", "A-Z")
+	cmd.Stdin = strings.NewReader("some input")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("in all caps: %q\n", out.String())　　//in all caps: "SOME INPUT"
+}
