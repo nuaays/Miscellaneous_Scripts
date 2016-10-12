3d5a5ff0c014186e6228ac78572a272829490233
root <nuaays@gmail.com>
2016-10-12 16:58:38 +0800

    add route /api/user/create for create user and insert into mysql database

--- LearningGolang/MasteringGoWebServices/createUser.go
+++ LearningGolang/MasteringGoWebServices/createUser.go
@@ -6,40 +6,62 @@ import (
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
 )
 
-
 type User struct {
-  ID int "json:id"
-  Name  string "json:username"
-  Email string "json:email"
-  First string "json:first"
-  Last  string "json:last"
+	ID    int    "json:id"
+	Name  string "json:username"
+	Email string "json:email"
+	First string "json:first"
+	Last  string "json:last"
 }
-
-
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
 
-  NewUser := User{}
-  NewUser.Name = r.FormValue("user")
-  NewUser.Email = r.FormValue("email")
-  NewUser.First = r.FormValue("first")
-  NewUser.Last = r.FormValue("last")
-  output, err := json.Marshal(NewUser)
-  fmt.Println(string(output))
-  if err != nil {
-    fmt.Println("Something went wrong!")
-  }
+	NewUser := User{}
+	NewUser.Name = r.FormValue("user")
+	NewUser.Email = r.FormValue("email")
+	NewUser.First = r.FormValue("first")
+	NewUser.Last = r.FormValue("last")
+	output, err := json.Marshal(NewUser)
+	fmt.Println(string(output))
+	if err != nil {
+		fmt.Println("Something went wrong!")
+	}
 
-  sql := "INSERT INTO users set user_nickname='" + NewUser.Name + "', user_first='" + NewUser.First + "', user_last='" + NewUser.Last + "', user_email='" + NewUser.Email + "'"
-  q, err := database.Exec(sql)
-  if err != nil {
-    fmt.Println(err)
-  }
-  fmt.Println(q)
+	database, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/social_network")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer database.Close()
+
+	sql := "INSERT INTO users set user_nickname='" + NewUser.Name + "', user_first='" + NewUser.First + "', user_last='" + NewUser.Last + "', user_email='" + NewUser.Email + "'"
+	q, err := database.Exec(sql)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(q)
 }
 
-routes := mux.NewRouter()
-routes.HandleFunc("/api/user/create", CreateUser).Methods("GET")
+func main() {
+	gorillaRoute := mux.NewRouter()
+	//gorillaRoute.HandleFunc("/api/{user:[0-9]+}", Hello)
+	//gorillaRoute.HandleFunc("/api/{user:\\w+}", Hello)
+
+	//create user
+	//http://localhost:8080/api/user/create?user=nkozyra&first=Nathan&last=Kozyra&email=nathan@nathankozyra.com
+	gorillaRoute.HandleFunc("/api/user/create", CreateUser).Methods("GET")
+	http.Handle("/", gorillaRoute)
+
+	// Server := Server {
+	//   Addr: ":8080",
+	//   Handler: urlHandler,
+	//   ReadTimeout: 1000 * time.MicroSecond,
+	//   WriteTimeout: 1000 * time.MicroSecond,
+	//   MaxHeaderBytes: 0,
+	//   TLSConfig: nil
+	// }
+	http.ListenAndServe(":8080", nil)
+}
