450891f351eac3b44f656ff81195e0809571a725
root <nuaays@gmail.com>
2016-10-24 00:00:01 +0800

    add 404.html

--- LearningGolang/Go-Building-Web-Applications/test_gorilla_mux.go
+++ LearningGolang/Go-Building-Web-Applications/test_gorilla_mux.go
@@ -13,6 +13,10 @@ func pageHandler(w http.ResponseWriter, r *http.Request) {
   vars := mux.Vars(r)
   pageID := vars["id"]
   fileName := "files/" + pageID + ".html"
+  err := os.Stat(fileName)
+  if err != nil{
+    fileName = "files/404.html"
+  }
   http.ServeFile(w,r,fileName)
 }
 
