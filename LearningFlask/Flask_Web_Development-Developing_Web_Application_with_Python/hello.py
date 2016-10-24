0e111cc81827c51f9310e1036a1165779717efb5
root <nuaays@gmail.com>
2016-10-24 14:35:33 +0800

    add name

--- LearningFlask/Flask_Web_Development-Developing_Web_Application_with_Python/hello.py
+++ LearningFlask/Flask_Web_Development-Developing_Web_Application_with_Python/hello.py
@@ -3,7 +3,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "<h1>Hello World!</h1>"
+
+@app.route("/user/<name>")
+def user(name):
+    return "<h1>Hello, %s!</h1>" % name
+
+
 
 if __name__ == "__main__":
     app.run()
