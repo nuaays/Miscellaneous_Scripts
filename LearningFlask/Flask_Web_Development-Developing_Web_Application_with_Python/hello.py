395e1677192a8c3c81a782a8305e2e09b03c22e7
root <nuaays@gmail.com>
2016-10-24 14:45:43 +0800

    add make_response

--- LearningFlask/Flask_Web_Development-Developing_Web_Application_with_Python/hello.py
+++ LearningFlask/Flask_Web_Development-Developing_Web_Application_with_Python/hello.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from flask import request
+from flask import make_response
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     user_agent = request.headers.get('User-Agent')
-    return "<h1>Hello World!</h1>" + user_agent
+    response = make_response("<h1>This document carries a cookie!</h1><br>"+user_agent)
+    response.set_cookie('answer','42')
+    return response
+
+@app.route("/bad")
+def index():
+    return "<h1>Bad Request</h1>", 404
+
 
 @app.route("/user/<name>")
 def user(name):
