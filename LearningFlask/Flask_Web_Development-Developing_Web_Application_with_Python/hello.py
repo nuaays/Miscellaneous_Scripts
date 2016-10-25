fe8707ee2a9aa8f424ed54fd31a9a57a45ee8658
root <nuaays@gmail.com>
2016-10-25 12:10:55 +0800

    add SQLAlchemy

--- LearningFlask/Flask_Web_Development-Developing_Web_Application_with_Python/hello.py
+++ LearningFlask/Flask_Web_Development-Developing_Web_Application_with_Python/hello.py
@@ -1,23 +1,47 @@
 #!/usr/bin/env python
-#coding:utf-8
+# -*- coding=UTF-8 -*-
 #
+import os, sys
 from flask import Flask
 from flask import request, url_for, render_template
 from flask import make_response
 from flask import redirect
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm, Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
+from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
+
+
 app = Flask(__name__)
-app.debug = True
+app.config.from_object('config')
+db = SQLAlchemy(app)
+bootstrap = Bootstrap(app)
 manager = Manager(app)
 
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+class NameForm(Form):
+    name = StringField('What is your name', validators=[Required()])
+    submit = SubmitField('Submit')
+
 @app.route("/")
-def hello():
+def index():
     user_agent = request.headers.get('User-Agent')
-    response = make_response("<h1>This document carries a cookie!</h1><br>"+user_agent)
-    response.set_cookie('answer','42')
-    return response
+    # response = make_response("<h1>This document carries a cookie!</h1><br>"+user_agent)
+    # response.set_cookie('answer','42')
+    name = None
+    form = FlaskForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 @app.errorhandler(404)
 def page_not_found(e):
