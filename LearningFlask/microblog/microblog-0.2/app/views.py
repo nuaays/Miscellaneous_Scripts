1ebde4f1d111e902304d36137e6f7e6c465630ec
root <nuaays@gmail.com>
2016-10-26 11:47:35 +0800

    add microblog-0.2

---
+++ LearningFlask/microblog/microblog-0.2/app/views.py
@@ -0,0 +1,39 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = { 'nickname': 'Miguel' }
+    posts = [
+        { 
+            'author': { 'nickname': 'John' }, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': { 'nickname': 'Susan' }, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
+    return render_template("index.html",
+        title = 'Home',
+        user = user,
+        posts = posts)
+
+@app.route('/test')
+def test():
+    user = { 'nickname': 'Miguel' }
+    posts = [
+        { 
+            'author': { 'nickname': 'John' }, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': { 'nickname': 'Susan' }, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
+    return render_template("index.html",
+        #title = None,
+        user = user,
+        posts = posts)
