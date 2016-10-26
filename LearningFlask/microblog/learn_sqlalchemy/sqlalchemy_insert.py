c70ad30be626a738b0d7fcd01b6c4ecc62439c0f
root <nuaays@gmail.com>
2016-10-26 14:23:42 +0800

    learn sqlalchemy

---
+++ LearningFlask/microblog/learn_sqlalchemy/sqlalchemy_insert.py
@@ -0,0 +1,49 @@
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+ 
+from sqlalchemy_declarative import Address, Base, Person
+
+# class Person(Base):
+#     __tablename__ = 'person'
+#     # Here we define columns for the table person
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(250), nullable=False)
+ 
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
+
+engine = create_engine('sqlite:///sqlalchemy_example.db')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+ 
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+ 
+# Insert a Person in the person table
+new_person = Person(name='new person')
+session.add(new_person)
+session.commit()
+ 
+# Insert an Address in the address table
+new_address = Address(post_code='00000', person=new_person)
+session.add(new_address)
+session.commit()
